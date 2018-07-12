418aa3436ede59b1cf4590712de6122a396fe976
Maria Lobillo-Santos <maria.lobillo.santos@gmail.com>
2018-07-12 17:41:06 +0200

    arrow functions

---
+++ arrow-func/app.js
@@ -0,0 +1,13 @@
+
+const ten = _ => 10
+
+console.log(ten())
+
+const logger = log => console.log(log)
+logger('The logger function')
+
+const add = (a, b) => a + b
+console.log(add(2, 5))
+
+const multiply = (a, b) => a * b
+console.log(multiply(3, 5))
