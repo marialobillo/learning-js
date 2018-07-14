d223bb7db20035dc869beb8cb05f6c44a2cb244d
Maria Lobillo-Santos <maria.lobillo.santos@gmail.com>
2018-07-14 12:38:02 +0200

    objects

---
+++ objects/app.js
@@ -0,0 +1,21 @@
+
+let myobj = {}
+
+myobj = {
+  'color': 'blue'
+}
+
+myobj = {
+  color: 'green'
+}
+
+let color = myobj.color
+let material = myobj[color]
+
+myobject.aMethod = () => {
+  return 'Hello'
+}
+
+myobject.noMethod = (color) => {
+  return 'Orange'
+}
