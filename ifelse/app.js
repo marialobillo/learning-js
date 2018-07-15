b1694154acce45b89a6ffe86cea34d5cbf9ba542
Maria Lobillo-Santos <maria.lobillo.santos@gmail.com>
2018-07-15 13:37:30 +0200

    if else statement

---
+++ ifelse/app.js
@@ -0,0 +1,39 @@
+
+
+const james = 22
+const valerie = 25
+const kenneth = 27
+
+// 1.
+if(36 < james){
+  return true
+} else {
+  return false
+}
+
+// 2.
+if( 36 > valerie){
+  return true
+} else {
+  return false
+}
+
+// 3.
+if( 36 === kenneth){
+  return true
+} else {
+  return false
+}
+
+1. no
+2. Yes
+3. no
+4. no
+5. no
+6. yes
+7. no
+8. yes
+9. no
+10. yes
+11. no
+12. yes
